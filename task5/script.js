d0b21d6957ccc1ced79e129001421be58d1469f2
alexvanzhula <alex.vanzhula@gmail.com>
2022-12-14 02:17:13 +0300

    style projetc

---
+++ task5/script.js
@@ -0,0 +1,11 @@
+const usertext = document.querySelector('#usertext');
+
+usertext.addEventListener('click', function (event){
+
+this.textContent = usertext;
+this.duplicateFields = ('usertext');
+
+event.preventDefault();
+})
+
+button.onclick
